bfe7eed03671b502db8cbb80c25baedbe4bfdcf4
Allen <b03901014@ntu.edu.tw>
2018-11-21 12:16:37 +0800

    Add sample_code.py and task_1

---
+++ src/sample_code.py
@@ -0,0 +1,32 @@
+'''
+This is the sample code from the homework. You shold NOT modify this file.
+Instead, please copy this file to src/students/<your student ID>.py and
+edit it there.
+'''
+
+
+def task_1():
+    '''
+    Task 1: Basic Syntax and Flake8 Checker
+
+    Python uses indentations to separate blocks instead of backets.
+    Unlike most language (like C++), indentations in Python are required.
+
+    See https://www.python-course.eu/python3_blocks.php for some examples.
+
+    Flake8 (http://flake8.pycqa.org/en/latest/) could help you check these
+    syntax error. It also regular your coding style. For example, using
+    two whitespaces as indentation is allowed in Python. However, Flake8
+    will tell you it is an error "E111: indentation is not a multiple of four".
+    This is because when many people work on the same project, it would be
+    confusing if people are using different identation style.
+
+    Following the coding style in Flake8 is strongly suggested.
+
+    '''
+    # TODO: fix the syntax error for the following code
+    if true:
+      print ("Hello word")
+
+    # End of TODO (do not change the code below)
+    return True
