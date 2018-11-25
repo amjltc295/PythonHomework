37d93447921238e00f42cb4492f438aac88428bf
Ya-Liang Chang (Allen) <b03901014@ntu.edu.tw>
2018-11-25 17:05:42 +0800

    Add travis

--- src/autograder.py
+++ src/autograder.py
@@ -20,24 +20,29 @@ def parse_args():
         default=[1, 2, 3, 4, 5, 6, 7, 8],
         help="The tasks you want to test (dafault: all)"
     )
+
+    parser.add_argument(
+        '-all', action='store_true',
+        help="Grade all files in students/"
+    )
     args = parser.parse_args()
     return args
 
 
-def autograde(args):
+def autograde(student_id, tasks):
     ''' Grade tasks specified in args.'''
     # Get path of this file
     dir_path = os.path.dirname(os.path.realpath(__file__))
 
     # Check if student's file exists
-    student_file = os.path.join(dir_path, 'students', args.student_id + '.py')
+    student_file = os.path.join(dir_path, 'students', student_id + '.py')
     assert os.path.exists(student_file), f"{student_file} not exists"
 
     # Import student's file as module
-    student_module = importlib.import_module(f'students.{args.student_id}')  # NOQA
+    student_module = importlib.import_module(f'students.{student_id}')  # NOQA
 
     # Run each task
-    for task_id in args.tasks:
+    for task_id in tasks:
         logger.info(f"Testing Task {task_id}")
         # Use try-except to catch erros in order to run througth all tasks
         try:
@@ -48,4 +53,11 @@ def autograde(args):
 
 if __name__ == '__main__':
     args = parse_args()
-    autograde(args)
+    if args.all:
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        student_ids = os.listdir(os.path.join(dir_path, 'students'))
+        student_ids = [x[:-3] for x in student_ids if x[-3:] == '.py']
+        for student_id in student_ids:
+            autograde(student_id, args.tasks)
+    else:
+        autograde(args.student_id, args.tasks)
