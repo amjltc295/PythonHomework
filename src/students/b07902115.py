88a951474b87bfb1b7d0637df7c5f1f08f7ccd9a
JOms <b07902115@csie.ntu.edu.tw>
2018-11-26 11:47:15 +0800

    Task 5: Uploaded
    Former-commit-id: c508eba536594573f3fdee7b7ed01f14d632d3ad [formerly f1c621b2d12af2a7da990e93bc62c79208c075f2]
    Former-commit-id: 3a87fb42a0592ac51a4109afc85c40c4681a49b2

--- src/students/b07902115.py
+++ src/students/b07902115.py
@@ -257,7 +257,8 @@ def task_5(
             print(f"{line}")
         # TODO: read the content of the input file, where words are separate by
         # commas. Please remove the commas and write words to the output file
-        pass
+            for substr in line.split(','):
+                fout.write(substr)
     # End of TODO
 
     with open(output_filename, 'r') as fin:
