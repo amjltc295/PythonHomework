865174b2a5f4d7de278802516a2da61509a399e5
timothychen1999 <45344933+timothychen1999@users.noreply.github.com>
2018-11-27 16:47:03 +0800

    Update b07902025.py

--- src/students/b07902025.py
+++ src/students/b07902025.py
@@ -116,7 +116,7 @@ def task_2(
     '''
     # TODO: change length and sentence to fit the requirement
     length = len(input_list)
-    sentence = str(input_list[target_index])+str(input_dictionary[target_key])
+    sentence = str(input_list[target_index]) + str(input_dictionary[target_key])
     # End of TODO
     input_list_length_and_sentence = (length, sentence)
     print(input_list_length_and_sentence)
