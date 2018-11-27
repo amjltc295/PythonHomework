cf6d707d4dae97a414bbc3d225a4e27e15e40938
Adam Su <b03502077@ntu.edu.tw>
2018-11-27 23:29:47 +0800

    Finish task_6
    Former-commit-id: 670cce391150711a6dbfe5f42197b155302c67ff [formerly 1ed8ad31b22e311d7a39be12e00306f36a40a4be]
    Former-commit-id: f53596da036b12e9cc72c0f2680ffce3f5cda25a

--- src/students/b03502077.py
+++ src/students/b03502077.py
@@ -312,7 +312,7 @@ def task_6(
         '''
         # TODO: use the above functions to calculate cosine similarity of
         # the two vectors v1 and v2
-        cos_sim = 0
+        cos_sim = dot_product(v1, v2) / (norm(v1) * norm(v2))
         # End of TODO
 
         return cos_sim
