ef16ad6dbc7ed6d08dfd87da42071871fd03ca5a
James <b07902013@ntu.edu.tw>
2018-11-27 10:40:04 +0800

    use the given function to complete the calculation.
    Former-commit-id: 77d5ba261b7fdece284af88f47bc26fc5c7416f7 [formerly 419471bb725570e46f912ad8fb20062050b65f2b]
    Former-commit-id: c8e5a860254e6590915d3a8403d578a0b2c160e4

--- src/students/b07902013.py
+++ src/students/b07902013.py
@@ -213,7 +213,7 @@ def task_4(
         j = 1
         while j <= numbers[i]:
             stars = "*"
-            list_of_stars_while.append(stars * j) # This line should be prior to j += 1.
+            list_of_stars_while.append(stars * j)
             j += 1  # This line is equivalant to j = j + 1
         i += 1
         # End of TODO
@@ -309,7 +309,7 @@ def task_6(
         '''
         # TODO: use the above functions to calculate cosine similarity of
         # the two vectors v1 and v2
-        cos_sim = 0
+        cos_sim = dot_product(v1, v2) / (norm(v1) * norm(v2))
         # End of TODO
 
         return cos_sim
