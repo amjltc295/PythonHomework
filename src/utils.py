b0de42e56d73c50b9523b4e442c1bc1492b7a51a
edisonhello <edisonhello@hotmail.com>
2018-11-27 20:28:03 +0800

    [autograder] move judge to util

--- src/utils.py
+++ src/utils.py
@@ -3,6 +3,7 @@ Provides some helped functions
 '''
 
 from PIL import Image, ImageFont, ImageDraw
+import math
 
 
 def draw_text(
@@ -15,3 +16,11 @@ def draw_text(
     font = ImageFont.truetype("arial.ttf", 20)
     draw.text(location, text, font=font, fill=text_color)
     return img
+
+
+def floating_judge(a, b, tol=1e-6):
+    assert len(a) == len(b), "Length of the output and answer does not match"
+    for i in range(len(a)):
+        if not math.isclose(a[i], b[i], rel_tol=tol):
+            return False
+    return True
