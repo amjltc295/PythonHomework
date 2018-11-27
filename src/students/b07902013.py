d422be7402d5070d0f249dc3ae4614bbfcf67815
James <b07902013@ntu.edu.tw>
2018-11-27 11:14:19 +0800

    use split and a loop to write the output file.

--- src/students/b07902013.py
+++ src/students/b07902013.py
@@ -262,7 +262,9 @@ def task_5(
             print(f"{line}")
         # TODO: read the content of the input file, where words are separate by
         # commas. Please remove the commas and write words to the output file
-        pass
+            new = line.split(",")
+            for get in new:
+                fout.write(get)
     # End of TODO
 
     with open(output_filename, 'r') as fin:
