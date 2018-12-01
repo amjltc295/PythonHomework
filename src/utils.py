f4a3952fe6f9239360867f2a2155c79c928db884
Estarriol1999 <b07902103@ntu.ede.tw>
2018-12-01 22:42:19 +0800

    add utils back

---
+++ src/utils.py
@@ -0,0 +1,27 @@
+'''
+Provides some helped functions
+'''
+
+import math
+
+from PIL import Image, ImageFont, ImageDraw
+
+
+def draw_text(
+    img: Image,
+    text: str,
+    location: tuple = (0, 0),
+    text_color=(0, 0, 0)
+) -> Image:
+    draw = ImageDraw.Draw(img)
+    font = ImageFont.truetype("DejaVuSans.ttf", 20)
+    draw.text(location, text, font=font, fill=text_color)
+    return img
+
+
+def floating_judge(a, b, tol=1e-6):
+    assert len(a) == len(b), "Length of the output and answer does not match"
+    for i in range(len(a)):
+        if not math.isclose(a[i], b[i], rel_tol=tol):
+            return False
+    return True
