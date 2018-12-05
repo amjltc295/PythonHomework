d1867d29a85ce0578e9351a376cfeec52b9e14b9
Joseph Tsai <josephtsai0124@gmail.com>
2018-12-05 21:15:04 +0800

    doing task 5

--- src/students/B07902017.py
+++ src/students/B07902017.py
@@ -257,7 +257,7 @@ def task_5(
             print(f"{line}")
         # TODO: read the content of the input file, where words are separate by
         # commas. Please remove the commas and write words to the output file
-        pass
+        fout.write(lines.split(','))
     # End of TODO
 
     with open(output_filename, 'r') as fin:
