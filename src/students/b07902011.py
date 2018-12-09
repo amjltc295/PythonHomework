125ca973fbf465a76617495a8f6c408f0ce063e1
waffle <b07902011@ntu.edu.tw>
2018-12-10 01:37:47 +0800

    finish all tasks
    Former-commit-id: 76001f7bb5218f164bc3aadede7f041e51823c89 [formerly bfa18d4683ba00c73fc1fc099a8866e8a6693225]
    Former-commit-id: 5a1e0dcddbd308e36456de52398ca22ff4ae2552

--- src/students/b07902011.py
+++ src/students/b07902011.py
@@ -10,7 +10,7 @@ SRC_PATH = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 TEST_DATA_DIR = os.path.join(SRC_PATH, 'test_data')
 
 
-def task_1():
+def task_1(dummy=None):
     '''
     Task 1: Basic Syntax and Flake8 Checker
 
@@ -31,8 +31,8 @@ def task_1():
 
     '''
     # Hint:
-    # Run `python src/autograder.py -task 1 -student_id <your student_id ID>`
-    # to see if you pass this task.
+    # Run `python autograder.py -task 1 -student_id <your student ID>`
+    # under src/ to see if you pass this task.
     # The correct output would be "Hello world" without any
     # error. Note that passing this task does NOT mean you pass the
     # Flake8 chcker. Please check your style with
@@ -40,8 +40,8 @@ def task_1():
 
     # TODO: fix the syntax error for the following code
     if True:
-        sentence="Hello world"
-        print (sentence)
+        sentence = "Hello world"
+        print(sentence)
 
     # End of TODO (do not change the code below)
     return True
@@ -112,11 +112,11 @@ def task_2(
         to string.
         * Run `python src/autograder.py -task 2 -student_id <your student ID>`
         to see if you pass this task.
-        * The correct output would be (5, '1 taiwan')
+        * The correct output would be (4, '1 taiwan')
     '''
     # TODO: change length and sentence to fit the requirement
-    length = None
-    sentence = None
+    length = len(input_list)
+    sentence = str(input_list[target_index]) + input_dictionary[target_key]
     # End of TODO
     input_list_length_and_sentence = (length, sentence)
     print(input_list_length_and_sentence)
@@ -145,21 +145,22 @@ def task_3(
     '''
     prime_factors_below_10 = []
     # TODO: fill in the conditions
-    if "some condition here":
+    if number < 0:
         prime_factors_below_10 = [-1]
     # elif stands for "else if" in Python.
-    elif "some condition here":
+    elif number == 0:
         prime_factors_below_10 = [0]
     else:
-        if "some condition here":
+        if number % 2 == 0:
             prime_factors_below_10.append(2)
-        if "some condition here":
+        if number % 3 == 0:
             prime_factors_below_10.append(3)
-        if "some condition here":
+        if number % 5 == 0:
             prime_factors_below_10.append(5)
-        if "some condition here":
+        if number % 7 == 0:
             prime_factors_below_10.append(7)
     # End of TODO
+    print(prime_factors_below_10)
     return prime_factors_below_10
 
 
@@ -200,19 +201,19 @@ def task_4(
     # In Python, the for loop could iterate through a list directly
     for number in numbers:
         # TODO: change stars to correct length
-        for i in range(1, number+1):
-            stars = "*"
+        for i in range(1, number + 1):
+            stars = "*" * i
             list_of_stars.append(stars)
         # End of TODO
 
     # This could be done by the while loop
     list_of_stars_while = []
     i = 0
-    while i <= len(numbers):
+    while i < len(numbers):
         # TODO: change stars to correct length
         j = 1
         while j <= numbers[i]:
-            stars = "*"
+            stars = "*" * j
             j += 1  # This line is equivalant to j = j + 1
             list_of_stars_while.append(stars)
         i += 1
@@ -231,9 +232,9 @@ def task_4(
 
 
 def task_5(
-    input_filename: str = os.path.join(TEST_DATA_DIR, 'task_5_input.txt'),
-    output_filename: str = os.path.join(TEST_DATA_DIR, 'task_5_output.txt')
-) -> list:
+    input_filename: str = 'task_5_input.txt',
+    output_filename: str = 'task_5_output.txt'
+) -> str:
     '''
     Task 5: I/O with files
 
@@ -249,6 +250,12 @@ def task_5(
         * Use fout.write(something) to write text into the output file
 
     '''
+    input_filename = os.path.join(TEST_DATA_DIR, input_filename)
+    output_filename = os.path.join(TEST_DATA_DIR, output_filename)
+    # Remove previous output file
+    if os.path.exists(output_filename):
+        os.remove(output_filename)
+
     with open(input_filename, 'r') as fin, open(output_filename, 'w') as fout:
         lines = fin.readlines()
         print(f"=======> Input file content:")
@@ -256,15 +263,16 @@ def task_5(
             print(f"{line}")
         # TODO: read the content of the input file, where words are separate by
         # commas. Please remove the commas and write words to the output file
-        pass
+            list = line.split(',')
+            for i in list:
+                fout.write(i)
     # End of TODO
 
     with open(output_filename, 'r') as fin:
         lines = fin.readlines()
         print(f"=======> Output file content:")
-        for line in lines:
-            print(f"{line}")
-        return lines
+        print(lines)
+        return "".join(lines)
 
 
 def task_6(
@@ -304,7 +312,7 @@ def task_6(
         '''
         # TODO: use the above functions to calculate cosine similarity of
         # the two vectors v1 and v2
-        cos_sim = 0
+        cos_sim = dot_product(v1, v2) / (norm(v1) * norm(v2))
         # End of TODO
 
         return cos_sim
@@ -353,7 +361,8 @@ def task_7(
         * Use <created object>.<object function> to call object function
     '''
     # TODO: create a student object with different words to say
-    student = None
+    student = Student(student_id, time)
+    student.set_words_to_say("hi")
     # End of TODO
 
     print(student.hello())
@@ -378,10 +387,14 @@ def task_8(
         * You could easily find answers with Google
     '''
     from urllib import request
-    result_img = None
+    import utils
+    import io
+    store = request.urlopen(img_url)
+    result_img = Image.open(io.BytesIO(store.read()))
+    result_img = utils.draw_text(result_img, "B07902011")
 
     # TODO: download the image from img_url with the request module
-    # and add your student ID on it with draw_name() in the utils module
+    # and add your student ID on it with draw_text() in the utils module
     # under src/.
 
     # You are allowed to change the img_url to your own image URL.
