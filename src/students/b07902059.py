4529eb9248631fcb964f14c59e3c3ffa022ae745
b07902059 <b07902059@ntu.edu.tw>
2018-12-09 19:39:30 +0800

    task 4
    Former-commit-id: 7e35b471d9b3c377605e093802832bbb5721e88d [formerly 9c17992130aa33eb6a14c79eee21675968154f8d]
    Former-commit-id: 627fbd9cf9fcc44165ec5e4ea9ed7b3b0dbe4f3b

--- src/students/b07902059.py
+++ src/students/b07902059.py
@@ -202,7 +202,7 @@ def task_4(
     for number in numbers:
         # TODO: change stars to correct length
         for i in range(1, number+1):
-            stars = "*"
+            stars = "*" * i
             list_of_stars.append(stars)
         # End of TODO
 
@@ -213,7 +213,7 @@ def task_4(
         # TODO: change stars to correct length
         j = 1
         while j <= numbers[i]:
-            stars = "*"
+            stars = "*" * j
             j += 1  # This line is equivalant to j = j + 1
             list_of_stars_while.append(stars)
         i += 1
