13ebcf23c3fae7f0e852c2443aacc7bf3a426017
Shine0417 <b07902109@ntu.edu.tw>
2018-12-12 01:02:36 +0800

    Finish all and import urlibs.py
    Former-commit-id: 57ecf14edbc55a42212095e9c75dfa2e7422aad5 [formerly fd3d6d5b6a3b8479f71e3068c740389b34526826]
    Former-commit-id: 23dd2128ba55fe8f18c4044adb97504ec0968e46

--- src/students/b07902109.py
+++ src/students/b07902109.py
@@ -394,16 +394,7 @@ def task_8(
     # under src/.
     from urllib import request
     from PIL import Image, ImageFont, ImageDraw
-    def draw_text(
-         img: Image,
-         text: str,
-         location: tuple = (0, 0),
-         text_color=(0, 0, 0)
-     ) -> Image:
-        draw = ImageDraw.Draw(img)
-        font = ImageFont.truetype("DejaVuSans.ttf", 20)
-        draw.text(location, text, font=font, fill=text_color)
-        return img
+    from utils import draw_text
     result_img = None
     local_filename, headers = request.urlretrieve(img_url)
     img = Image.open(local_filename)
